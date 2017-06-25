ffb395c92722c58c05355e552fd34c921d48b94d
deepansh90 <deepansh.rawal2@gmail.com>
2017-06-25 17:02:17 +0530

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+
+        window.plugins.AdMob.setOptions({
+            publisherId: "ca-app-pub-6030569595197388/8625778957",
+            interstitialAdId: "ca-app-pub-6030569595197388/1102512157",
+            bannerAtTop: false, // set to true, to put banner at top
+            overlap: false, // set to true, to allow banner overlap webview
+            offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+            isTesting: false, // receiving test ad
+            autoShow: true // auto show interstitial ad when loaded
+        });
+        // display the banner at startup
+        window.plugins.AdMob.createBannerView();
+
+        // create interstitial ad
+        window.plugins.AdMob.createInterstitialView();
+  
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
